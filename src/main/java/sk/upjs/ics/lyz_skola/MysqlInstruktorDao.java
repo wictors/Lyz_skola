0050041133b38ec59eae748b4d6a14ecf46b2c43
Wictor <Silvia@Wictor-PC>
2016-11-25 19:52:39 +0100

    2. UPLOAD MysqlInstruktorDao (nezmeneny)

---
+++ src/main/java/sk/upjs/ics/lyz_skola/MysqlInstruktorDao.java
@@ -0,0 +1,27 @@
+package sk.upjs.ics.lyz_skola;
+
+import java.util.List;
+
+public class MysqlInstruktorDao implements InstruktorDao {
+
+    @Override
+    public List<Instruktor> dajInstruktorov() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void pridajInstruktora(Instruktor instruktor) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Instruktor> podlaPirezviska(String priezvisko) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Instruktor podlaId(Long id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
