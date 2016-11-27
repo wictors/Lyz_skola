3d3e441402144255aa016168c40c3c9fd3b01375
Hej urcite <Hej urcite@Okay>
2016-11-27 09:38:53 +0100

    Vytvorenie MysqlZakaznikDao, doplnenie metod MysqlInstruktorDao.

--- src/main/java/sk/upjs/ics/lyz_skola/MysqlInstruktorDao.java
+++ src/main/java/sk/upjs/ics/lyz_skola/MysqlInstruktorDao.java
@@ -1,22 +1,37 @@
 package sk.upjs.ics.lyz_skola;
 
 import java.util.List;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 
 public class MysqlInstruktorDao implements InstruktorDao {
 
+    private JdbcTemplate jdbcTemplate;
+
+    public MysqlInstruktorDao(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+            
     @Override
     public List<Instruktor> dajInstruktorov() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        String sql = "SELECT * FROM instruktor";
+        BeanPropertyRowMapper<Instruktor> mapper = BeanPropertyRowMapper.newInstance(Instruktor.class);
+        return jdbcTemplate.query(sql, mapper);
     }
 
     @Override
     public void pridajInstruktora(Instruktor instruktor) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        jdbcTemplate.update("INSERT INTO instruktor(meno, priezvisko, email,"
+                + "akreditacia, typ, heslo) VALUES(?,?,?)", instruktor.getMeno(),
+                instruktor.getPriezvisko(), instruktor.getEmail(), instruktor.getAkreditacia(),
+                instruktor.getTyp(), instruktor.getHeslo());
     }
 
     @Override
     public List<Instruktor> podlaPriezviska(String priezvisko) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        String sql = "SELECT * FROM hodina WHERE priezvisko = ?";
+        BeanPropertyRowMapper<Instruktor> mapper = BeanPropertyRowMapper.newInstance(Instruktor.class);
+        return jdbcTemplate.query(sql, mapper, priezvisko);
     }
 
     @Override
